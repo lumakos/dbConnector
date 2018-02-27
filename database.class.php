0bb113277d149ae9e6d639eba10a7df90d54f4c0
lumakos <lumakos23@gmail.com>
2018-02-27 22:46:02 +0200

    add CRUD

--- database.class.php
+++ database.class.php
@@ -81,7 +81,7 @@
          * @param string name of the table
          * @param array select, where, order_by, limit and return_type conditions
          */
-        public function selectData($table,$conditions = array())
+        public function selectData($table, $conditions = array())
         {
             $sql = 'SELECT ';
             $sql .= array_key_exists("select",$conditions)?$conditions['select']:'*';
@@ -106,6 +106,7 @@
             if(array_key_exists("start",$conditions) && array_key_exists("limit",$conditions))
             {
                 $sql .= ' LIMIT '.$conditions['start'].','.$conditions['limit']; 
+
             } elseif(!array_key_exists("start",$conditions) && array_key_exists("limit",$conditions))
             {
                 $sql .= ' LIMIT '.$conditions['limit']; 
@@ -135,6 +136,111 @@
                 }
             }
             return !empty($data)?$data:false;
+        }
+
+        /**
+         * Insert data into the database
+         * @param string name of the table
+         * @param array the data for inserting into the table
+         */
+        public function insertData($table,$data)
+        {
+            if(!empty($data) && is_array($data))
+            {
+                $columns = '';
+                $values  = '';
+                $i = 0;
+                // if(!array_key_exists('created',$data))
+                // {
+                //     $data['created'] = date("Y-m-d H:i:s");
+                // }
+                // if(!array_key_exists('modified',$data))
+                // {
+                //     $data['modified'] = date("Y-m-d H:i:s");
+                // }
+
+                $columnString = implode(',', array_keys($data));
+                $valueString = ":".implode(',:', array_keys($data));
+                $sql = "INSERT INTO ".$table." (".$columnString.") VALUES (".$valueString.")";
+                $query = $this->connection->prepare($sql);
+                foreach($data as $key=>$val)
+                {
+                     $query->bindValue(':'.$key, $val);
+                }
+                $insert = $query->execute();
+                return $insert?$this->connection->lastInsertId():false;
+            } else
+            {
+                return false;
+            }
+        }
+        
+        /**
+         * Update data into the database
+         * @param string name of the table
+         * @param array the data for updating into the table
+         * @param array where condition on updating data
+         */
+        public function updateData($table, $data, $conditions)
+        {
+            if(!empty($data) && is_array($data))
+            {
+                $colvalSet = '';
+                $whereSql = '';
+                $i = 0;
+                // if(!array_key_exists('modified',$data))
+                // {
+                //     $data['modified'] = date("Y-m-d H:i:s");
+                // }
+                foreach($data as $key=>$val)
+                {
+                    $pre = ($i > 0)?', ':'';
+                    $colvalSet .= $pre.$key."='".$val."'";
+                    $i++;
+                }
+                if(!empty($conditions)&& is_array($conditions))
+                {
+                    $whereSql .= ' WHERE ';
+                    $i = 0;
+                    foreach($conditions as $key => $value)
+                    {
+                        $pre = ($i > 0)?' AND ':'';
+                        $whereSql .= $pre.$key." = '".$value."'";
+                        $i++;
+                    }
+                }
+                $sql = "UPDATE ".$table." SET ".$colvalSet.$whereSql;
+                $query = $this->connection->prepare($sql);
+                $update = $query->execute();
+                return $update?$query->rowCount():false;
+            } else
+            {
+                return false;
+            }
+        }
+        
+        /**
+         * Delete data from the database
+         * @param string name of the table
+         * @param array where condition on deleting data
+         */
+        public function deleteData($table, $conditions)
+        {
+            $whereSql = '';
+            if(!empty($conditions)&& is_array($conditions))
+            {
+                $whereSql .= ' WHERE ';
+                $i = 0;
+                foreach($conditions as $key => $value)
+                {
+                    $pre = ($i > 0)?' AND ':'';
+                    $whereSql .= $pre.$key." = '".$value."'";
+                    $i++;
+                }
+            }
+            $sql = "DELETE FROM ".$table.$whereSql;
+            $delete = $this->connection->exec($sql);
+            return $delete?$delete:false;
         }
 
     }
--- index.php
+++ index.php
@@ -14,10 +14,28 @@
     $rows = $db->selectData('MyGuests', array('order_by'=>'id DESC'));
     if(!empty($rows)) { 
         foreach($rows as $row) { 
-            echo $ow['firstname']; 
+            echo $row['firstname']; 
         }
     }
-    
+
+    /* Insert data */
+    $tblName = "MyGuests";
+    $userData = array(
+            'firstname' => 'yiannis'
+        );
+    $db->insertData($tblName,$userData);
+
+    /* Update data */
+    $userData1 = array(
+                'firstname' => 'yiannis update'
+            );
+    $condition = array('id' => 3);
+    $db->updateData($tblName,$userData1,$condition);
+
+    /* Delete data */
+    $condition = array('id' => 3);
+    $db->deleteData($tblName,$condition);
+
     /* Kill connection */
     $db->closeConnection();
 ?>
